9bd066b941452a224e481c2cc7f0479fb41d7c11
Eduardo Rosas <lalorosas@outlook.com>
2020-10-05 22:17:52 -0500

    The View Model

---
+++ EvernoteClone/ViewModel/Commands/NewNoteCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+    public class NewNoteCommand : ICommand
+    {
+        public NotesVM ViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public NewNoteCommand(NotesVM vm)
+        {
+            ViewModel = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO: create new note
+        }
+    }
+}
---
+++ EvernoteClone/ViewModel/NotesVM.cs
@@ -0,0 +1,33 @@
+﻿using EvernoteClone.Model;
+using EvernoteClone.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EvernoteClone.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+        public ObservableCollection<Note> Notes { get; set; }
+        private Notebook selectedNotebook;
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                //TODO: get notes for this selectedNotebook
+            }
+        }
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNoteCommand = new NewNoteCommand(this);
+            NewNotebookCommand = new NewNotebookCommand(this);
+        }
+    }
+}
