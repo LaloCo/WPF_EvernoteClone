93a4e784177ef993c768fee09b9c51f95048c7bf
Eduardo Rosas <lalorosas@outlook.com>
2020-10-14 23:31:35 -0500

    Speech recognizer .NET Core

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -27,9 +29,20 @@ namespace EvernoteClone.View
             Application.Current.Shutdown();
         }
 
-        private void speechButton_Click(object sender, RoutedEventArgs e)
+        private async void speechButton_Click(object sender, RoutedEventArgs e)
         {
+            string region = "southcentralus";
+            string key = "87464598e855488dbe3f5c85b71005f2";
 
+            var speechConfig = SpeechConfig.FromSubscription(key, region);
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+                {
+                    var result = await recognizer.RecognizeOnceAsync();
+                    contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
+                }
+            }
         }
 
         private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
