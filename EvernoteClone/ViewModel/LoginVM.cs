b3f9974a1389ef6f998324d367a433eeee9f4468
Eduardo Rosas <lalorosas@outlook.com>
2020-10-30 23:23:26 -0600

    Register view complete

--- EvernoteClone/ViewModel/LoginVM.cs
+++ EvernoteClone/ViewModel/LoginVM.cs
@@ -16,7 +16,106 @@ namespace EvernoteClone.ViewModel
         public User User
         {
             get { return user; }
-            set { user = value; }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                User = new User
+                {
+                    Username = username,
+                    Password = this.Password,
+                    Name = this.Name,
+                    Lastname = this.Lastame,
+                    ConfirmPassword = this.ConfirmPassword
+                };
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                User = new User
+                {
+                    Username = this.Username,
+                    Password = password,
+                    Name = this.Name,
+                    Lastname = this.Lastame,
+                    ConfirmPassword = this.ConfirmPassword
+                };
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                User = new User
+                {
+                    Username = this.Username,
+                    Password = this.Password,
+                    Name = name,
+                    Lastname = this.Lastame,
+                    ConfirmPassword = this.ConfirmPassword
+                };
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string lastname;
+        public string Lastame
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                User = new User
+                {
+                    Username = this.Username,
+                    Password = this.Password,
+                    Name = this.Name,
+                    Lastname = lastname,
+                    ConfirmPassword = this.ConfirmPassword
+                };
+                OnPropertyChanged("Lastame");
+            }
+        }
+
+        private string confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return confirmPassword; }
+            set
+            {
+                confirmPassword = value;
+                User = new User
+                {
+                    Username = this.Username,
+                    Password = this.Password,
+                    Name = this.Name,
+                    Lastname = this.Lastame,
+                    ConfirmPassword = confirmPassword
+                };
+                OnPropertyChanged("ConfirmPassword");
+            }
         }
 
         private Visibility loginVis;
@@ -55,6 +154,8 @@ namespace EvernoteClone.ViewModel
             RegisterCommand = new RegisterCommand(this);
             LoginCommand = new LoginCommand(this);
             ShowRegisterCommand = new ShowRegisterCommand(this);
+
+            User = new User();
         }
 
         public void SwitchViews()
@@ -71,6 +172,16 @@ namespace EvernoteClone.ViewModel
                 RegisterVis = Visibility.Collapsed;
                 LoginVis = Visibility.Visible;
             }
+        }
+
+        public void Login()
+        {
+            // TODO: login
+        }
+
+        public void Register()
+        {
+            // TODO: Register
         }
 
         private void OnPropertyChanged(string propertyName)
